666197b6c5f9024a559a269a17858d0fee483250
Jeff Espenschied <jespenschied@gmail.com>
2012-05-04 06:33:27 -0400

    Drop log4net and put in dummy for now.

--- PersistentClipboard/GlobalHotkey.cs
+++ PersistentClipboard/GlobalHotkey.cs
@@ -32,12 +32,12 @@ namespace PersistentClipboard
 
         internal delegate void HandleKeyDown();
 
-        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        private IntPtr SetHook(LowLevelKeyboardProc keyboardProc)
         {
             using (Process curProcess = Process.GetCurrentProcess())
             using (ProcessModule curModule = curProcess.MainModule)
             {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                return SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, GetModuleHandle(curModule.ModuleName), 0);
             }
         }
 
@@ -56,17 +56,17 @@ namespace PersistentClipboard
 
         private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
---
+++ PersistentClipboard/SimpleLogger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersistentClipboard
+{
+    internal interface ILog
+    {
+        void Debug(string message);
+        void DebugFormat(string message, params object[] args);
+        void Info(string message);
+        void InfoFormat(string message, params object[] args);
+        void Error(string message);
+        void Error(string message, Exception e);
+        void ErrorFormat(string message, params object[] args);
+    }
+    
+    internal class SimpleLogger : ILog
+    {
+        private bool enabled = false;
+        public SimpleLogger(bool enable)
+        {
+            enabled = enable;
+        }
+
+        public void Debug(string message)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+
+        public void DebugFormat(string message, params object[] args)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+
+        public void Info(string message)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+
+        public void InfoFormat(string message, params object[] args)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+
+        public void Error(string message, Exception e)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+
+        public void ErrorFormat(string message, params object[] args)
+        {
+            if (enabled)
+                throw new NotImplementedException();
+        }
+    }
+}
