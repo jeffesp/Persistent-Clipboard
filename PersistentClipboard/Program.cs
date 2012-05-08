09a258a20ea13bac00856c029b061e58f3423c1b
Jeff Espenschied <jespenschied@gmail.com>
2012-05-08 19:02:42 -0400

    Make so only one instance can run at a time.

--- PersistentClipboard/Program.cs
+++ PersistentClipboard/Program.cs
@@ -6,29 +6,37 @@ namespace PersistentClipboard
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{1A31407E-EDFB-43B8-95B5-D288C9052622}");
         public static ILog Logger { get; set; }
 
         [STAThread]
         static void Main()
         {
-
-            Logger = new SimpleLogger(true);
-
-            Application.ThreadException += new ThreadExceptionEventHandler(new ThreadExceptionHandler().ApplicationThreadException);
-
-            try
+            if (mutex.WaitOne(TimeSpan.Zero, true))
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-            }
-            catch (Exception e)
-            {
-                // Probably want something a little more sophisticated than this
-                MessageBox.Show(e.Message, "The application could not initialize.", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
-            }
+                Logger = new SimpleLogger(true);
 
-            Application.Run(new HostForm(Logger));
+                Application.ThreadException += new ThreadExceptionEventHandler(new ThreadExceptionHandler().ApplicationThreadException);
+
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                }
+                catch (Exception e)
+                {
+                    // Probably want something a little more sophisticated than this
+                    MessageBox.Show(e.Message, "The application could not initialize.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(0);
+                }
+
+                Application.Run(new HostForm(Logger));
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("Another instance of Persistent Clipboard is already running.");
+            }
         }
     }
 
