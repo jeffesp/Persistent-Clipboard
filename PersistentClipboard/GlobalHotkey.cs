87fe4d99fe2e609c0cc887aeafcecec6705bafc9
Jeff Espenschied <jespenschied@gmail.com>
2012-12-18 16:03:44 -0500

    Go back to our own handler as bug was not with it, but with the where it was created.

--- PersistentClipboard/GlobalHotkey.cs
+++ PersistentClipboard/GlobalHotkey.cs
@@ -1,47 +1,71 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
-
-using MouseKeyboardActivityMonitor;
-using MouseKeyboardActivityMonitor.WinApi;
-
-using SimpleLogger;
+using System.Runtime.InteropServices;
 
 namespace PersistentClipboard
 {
     internal class GlobalHotkey : IDisposable
     {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private readonly LowLevelKeyboardProc proc;
+        private readonly IntPtr hookId = IntPtr.Zero;
         private readonly HandleKeyDown keyHandleKeyDown;
         private readonly Keys watchKey;
         private readonly Keys watchModifers;
-        private readonly KeyboardHookListener listener;
 
         internal GlobalHotkey(HandleKeyDown handleKeyDown, Keys key, Keys modifiers)
         {
             keyHandleKeyDown = handleKeyDown;
             watchKey = key;
             watchModifers = modifiers;
-            listener = new KeyboardHookListener(new GlobalHooker());
-            listener.Enabled = true;
-            listener.KeyDown += HookCallback;
-        }
-
-        internal delegate void HandleKeyDown();
-
-        private void HookCallback(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == watchKey && Control.ModifierKeys == watchModifers)
-            {
-                keyHandleKeyDown();
-                e.Handled = true;
-            }
+            proc = HookCallback;
+            hookId = SetHook(proc);
         }
 
         public void Dispose()
         {
-            if (listener != null)
-                listener.Dispose();
+            UnhookWindowsHookEx(hookId);
         }
+
+        internal delegate void HandleKeyDown();
+
+        private IntPtr SetHook(LowLevelKeyboardProc keyboardProc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys k = (Keys)vkCode;
+                if (k == watchKey && Control.ModifierKeys == watchModifers)
+                    keyHandleKeyDown();
+            }
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
 
